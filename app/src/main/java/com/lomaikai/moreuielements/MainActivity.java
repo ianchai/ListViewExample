be1dc9ad5e079c022348b044fa0d1d7493d8becd
Ian Chai <ianchai@mmu.edu.my>
2021-02-18 12:30:20 +0800

    It just toasts the fruit selected.

--- app/src/main/java/com/lomaikai/moreuielements/MainActivity.java
+++ app/src/main/java/com/lomaikai/moreuielements/MainActivity.java
@@ -3,8 +3,11 @@ package com.lomaikai.moreuielements;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -38,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
         );
 
         fruitsList.setAdapter(fruitsAdapter);
+
+        fruitsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, fruits.get(i) + " selected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
